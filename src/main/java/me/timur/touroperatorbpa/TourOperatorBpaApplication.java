10e0e829307bfe0dceb92a9409d80161a6469ab7
t.ismoilov <t.ismoilov@paymo.uz>
2023-07-25 14:58:59 +0500

    first commit

---
+++ src/main/java/me/timur/touroperatorbpa/TourOperatorBpaApplication.java
@@ -0,0 +1,13 @@
+package me.timur.touroperatorbpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOperatorBpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourOperatorBpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/timur/touroperatorbpa/TourOperatorBpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.timur.touroperatorbpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOperatorBpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
