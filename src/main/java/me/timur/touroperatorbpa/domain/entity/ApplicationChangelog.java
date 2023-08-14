dab63c6fed0d70975a66ef14226d30faaa344e16
t.ismoilov <t.ismoilov@paymo.uz>
2023-08-14 19:09:32 +0500

    add ApplicationChangelog

---
+++ src/main/java/me/timur/touroperatorbpa/domain/entity/ApplicationChangelog.java
@@ -0,0 +1,36 @@
+package me.timur.touroperatorbpa.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import me.timur.touroperatorbpa.model.enums.ApplicationType;
+
+/**
+ * Created by Temurbek Ismoilov on 14/08/23.
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "application_changelog")
+public class ApplicationChangelog extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "application_type", nullable = false)
+    private ApplicationType applicationType;
+
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    @Column(name = "change", nullable = false)
+    private String change;
+
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false;
+}
---
+++ src/main/java/me/timur/touroperatorbpa/model/enums/ApplicationType.java
@@ -0,0 +1,15 @@
+package me.timur.touroperatorbpa.model.enums;
+
+/**
+ * Created by Temurbek Ismoilov on 14/08/23.
+ */
+
+public enum ApplicationType {
+    ACCOMMODATION,
+    FLIGHT,
+    TRAIN,
+    TRANSPORT,
+    RESTAURANT,
+    GUIDE,
+    OTHER,
+}
